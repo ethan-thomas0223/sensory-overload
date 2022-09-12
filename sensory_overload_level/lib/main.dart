faaa73f1f67cc421ca2355b0dd672094a7603416
Garrett <colliergc@hendrix.edu>
2022-09-12 09:09:27 -0500

    Add basic Gyroscope class with imports

--- sensory_overload_level/lib/main.dart
+++ sensory_overload_level/lib/main.dart
@@ -1,6 +1,8 @@
 //mobile sensory dart package package below:
 //https://pub.dev/packages/sensors_plus/install
 
+import 'dart:async';
+import 'package:sensors_plus/sensors_plus.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -52,6 +54,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
+// [GyroscopeEvent (x: 0.0, y: 0.0, z: 0.0)]
 
   void _incrementCounter() {
     setState(() {
@@ -113,6 +117,63 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Gyroscope extends StatefulWidget {
+  const Gyroscope({Key? key}) : super(key: key);
+
+  @override
+  _GyroscopeState createState() => _GyroscopeState();
+}
+
+class _GyroscopeState extends State<Gyroscope> {
+  double x = 0, y = 0, z = 0;
+  String direction = "none";
+
+  @override
+  void initState() {
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      print(event);
+
+      x = event.x;
+      y = event.y;
+      z = event.z;
+
+      //rough calculation, you can use
+      //advance formula to calculate the orentation
+      if (x > 0) {
+        direction = "back";
+      } else if (x < 0) {
+        direction = "forward";
+      } else if (y > 0) {
+        direction = "left";
+      } else if (y < 0) {
+        direction = "right";
+      }
+
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gyroscope Sensor"),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(30),
+          child: Column(children: [
+            Text(
+              direction,
+              style: const TextStyle(fontSize: 30),
+            )
+          ])),
     );
   }
 }
