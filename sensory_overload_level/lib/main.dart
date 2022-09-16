6932752b76a91dd38457ab45980078d914756742
Ethan Thomas <thomasem@hendrix.edu>
2022-09-15 20:17:05 -0500

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 6f456239b7a33b025b37bd3f9479f64c97b459b4.

--- sensory_overload_level/lib/main.dart
+++ sensory_overload_level/lib/main.dart
@@ -108,8 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
               width: 300, // <-- Your width
               height: 100,
               child: ElevatedButton(
-                onPressed: _openVertical,
-                child: const Text('Vertical Level'),
+                onPressed: _openHoizontal,
                 style: ElevatedButton.styleFrom(
                     textStyle: const TextStyle(fontSize: 30)),
                 child: const Text('Vertical Level'),
