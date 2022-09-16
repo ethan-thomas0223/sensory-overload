8a0dc2ef8270decc8e2ec32c56b1cc14b5b064a3
Ethan Thomas <thomasem@hendrix.edu>
2022-09-15 20:14:39 -0500

    Auto stash before merge of "master" and "origin/master"

--- sensory_overload_level/lib/main.dart
+++ sensory_overload_level/lib/main.dart
@@ -1,6 +1,8 @@
 //mobile sensory dart package package below:
 //https://pub.dev/packages/sensors_plus/install
 
+//add x animation for not level
+
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:checkmark/checkmark.dart';
@@ -28,15 +30,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Leveler',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.orange,
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ButtonStyle(
@@ -57,14 +50,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // Test Commit
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // This widget is the home page of your application.
 
   final String title;
 
@@ -75,7 +61,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // onPressed move to these states
+    // onPressed move to these states/pages
     void _openHoizontal() {
       Navigator.push(
         context,
@@ -114,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     textStyle: const TextStyle(fontSize: 30)),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 50,
             ),
             SizedBox(
@@ -146,6 +132,15 @@ class _HorizontalPageState extends State<HorizontalPage> {
   late double pitchH;
   late double rollH;
 
+  static const IconData cancel_outlined =
+      IconData(0xef28, fontFamily: 'MaterialIcons');
+  Icon openX() {
+    return Icon(
+      cancel_outlined,
+      color: Colors.red,
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -162,6 +157,7 @@ class _HorizontalPageState extends State<HorizontalPage> {
           checkedH = true;
         } else {
           checkedH = false;
+          openX();
         }
       },
     );
@@ -189,6 +185,19 @@ class _HorizontalPageState extends State<HorizontalPage> {
               ),
             ),
           ),
+          // Align(
+          //   alignment: Alignment.bottomCenter,
+          //   child: SizedBox(
+          //     height: 200,
+          //     width: 200,
+          //     //need to put an x here not a checkmark
+          //     child: CheckMark(
+          //       active: checkedH,
+          //       curve: Curves.bounceIn,
+          //       duration: const Duration(milliseconds: 500),
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
@@ -219,7 +228,7 @@ class _VerticalPageState extends State<VerticalPage> {
           },
         );
         // check if level vertically
-        if (pitchV <= 0.005 && pitchV >= -0.005) {
+        if (pitchV <= 0.03 && pitchV >= -0.03) {
           checkedV = true;
         } else {
           checkedV = false;
