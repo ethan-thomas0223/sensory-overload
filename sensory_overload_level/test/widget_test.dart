3830e8448b6f424c9c52b342347c28d029e66e93
mason-pAoLo <masonmillionaire725@gmail.com>
2022-09-19 12:22:49 -0500

    Unit Tests
    tests to make sure pages are properly filled

--- sensory_overload_level/test/widget_test.dart
+++ sensory_overload_level/test/widget_test.dart
@@ -1,4 +1,30 @@
+import 'package:checkmark/checkmark.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sensory_overload_level/main.dart';
 
-void main() {}
+/* Check for navigation and buttons
+*/
+
+void main() {
+  testWidgets('Horizontal Page has checkmark and X icon', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: HorizontalPage()));
+
+    expect(find.byType(CheckMark), findsOneWidget);
+    expect(find.byType(Icon), findsOneWidget);
+  });
+
+  testWidgets('Vertical Page has checkmark and X icon', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: VerticalPage()));
+
+    expect(find.byType(CheckMark), findsOneWidget);
+    expect(find.byType(Icon), findsOneWidget);
+  });
+
+  testWidgets('Home Page has 2 buttons', (tester) async {
+    await tester
+        .pumpWidget(const MaterialApp(home: MyHomePage(title: "Leveler")));
+
+    expect(find.byType(ElevatedButton), findsNWidgets(2));
+  });
+}
